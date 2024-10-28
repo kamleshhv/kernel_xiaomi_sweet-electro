7468b73c976a613fb19440a6cd44ccbc9e9041fb
kamleshhv <kamleshvansajalia@gmail.com>
2024-10-28 04:54:45 +0000

    Revert "ravb: Fix races between ravb_tx_timeout_work() and net related ops"
    This reverts commit 86c3bf62d274b65422584355d7fefde1e6d8f3f8.

--- drivers/net/ethernet/renesas/ravb_main.c
+++ drivers/net/ethernet/renesas/ravb_main.c
@@ -1484,12 +1484,6 @@ static void ravb_tx_timeout_work(struct work_struct *work)
 	struct net_device *ndev = priv->ndev;
 	int error;
 
-	if (!rtnl_trylock()) {
-		usleep_range(1000, 2000);
-		schedule_work(&priv->work);
-		return;
-	}
-
 	netif_tx_stop_all_queues(ndev);
 
 	/* Stop PTP Clock driver */
@@ -1522,7 +1516,7 @@ static void ravb_tx_timeout_work(struct work_struct *work)
 		 */
 		netdev_err(ndev, "%s: ravb_dmac_init() failed, error %d\n",
 			   __func__, error);
-		goto out_unlock;
+		return;
 	}
 	ravb_emac_init(ndev);
 
@@ -1532,9 +1526,6 @@ out:
 		ravb_ptp_init(ndev, priv->pdev);
 
 	netif_tx_start_all_queues(ndev);
-
-out_unlock:
-	rtnl_unlock();
 }
 
 /* Packet transmit function for Ethernet AVB */
